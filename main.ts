e34c3134be6684e880176f9da93911dcf5154bb4
Ilchenko <ilchenkowd@gmail.com>
2025-04-02 20:57:05 +0300

    добавление сервера и эндпоинта online/:id

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-function greet(name: string): string {
-	return `Hello, ${name}!`;
-}
+import { Application, Router } from "jsr:@oak/oak";
+import { getGameOnline } from "./api.ts";
 
-console.log(greet("world"));
+const router = new Router();
+
+router.get("/online/:id", async (ctx) => {
+	ctx.response.body = await getGameOnline(ctx.params.id);
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = 8369;
+console.log(`Server is running on http://localhost:${PORT}`);
+await app.listen({ port: PORT });
