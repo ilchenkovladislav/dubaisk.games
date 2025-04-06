39e7229f5390712c96b28f6cb6091349fb9d7e55
Ilchenko <ilchenkowd@gmail.com>
2025-04-06 11:19:08 +0300

    добавление парсинга online-fix

--- api.ts
+++ api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import iconv from "iconv-lite";
 
 export async function getGameOnline(id: number | string) {
 	const { data } = await axios.get(`https://steamcharts.com/app/${id}`);
@@ -24,4 +25,49 @@ export async function getGameOnline(id: number | string) {
 		gainPercent: $(gainPercent).text(),
 		peakPlayers: $(peakPlayers).text(),
 	});
+}
+
+function extractName(title: string): string {
+	const networkPart = " по сети";
+
+	const name = title.trim();
+	if (name.endsWith(networkPart)) {
+		return name.slice(0, name.length - networkPart.length);
+	}
+
+	return name;
+}
+
+function delay(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function getOnlineFixGames() {
+	const games = [];
+	const maxPages = 5;
+
+	for (let index = 1; index <= maxPages; index++) {
+		const { data } = await axios.get(`https://online-fix.me/page/${index}`, {
+			responseType: "arraybuffer",
+		});
+
+		const $ = cheerio.load(iconv.decode(data, "win1251"));
+
+		const pageGames = $(".news .article")
+			.toArray()
+			.map((el) => {
+				return {
+					link: $(el).find(".big-link").attr("href"),
+					name: extractName($(el).find(".title").text()),
+				};
+			});
+
+		games.push(...pageGames);
+
+		if (index < maxPages) {
+			await delay(1000);
+		}
+	}
+
+	return games;
 }
