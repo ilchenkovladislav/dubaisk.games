3aaefe7b7a6fdcc79d8b823f97fd55762eeefbe4
Ilchenko <ilchenkowd@gmail.com>
2025-04-06 11:19:36 +0300

    добавление эндпоинта onlinefix

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 import { Application, Router } from "jsr:@oak/oak";
-import { getGameOnline } from "./api.ts";
+import { getGameOnline, getOnlineFixGames } from "./api.ts";
 
 const router = new Router();
 
 router.get("/online/:id", async (ctx) => {
 	ctx.response.body = await getGameOnline(ctx.params.id);
+});
+
+router.get("/onlineFix", async (ctx) => {
+	ctx.response.body = await getOnlineFixGames();
 });
 
 const app = new Application();
